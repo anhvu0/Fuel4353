deac1a1a61132ef39504711bf15c263e2d445c51
RyanPham9x <jamesryan_9x@yahoo.com>
2024-02-22 12:21:46 -0600

    fuelquoteformv2

--- fuel_rate_calc/src/App.js
+++ fuel_rate_calc/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Login from './Login';
+import Register from './Register';
 import FuelQuoteForm from './FuelQuoteForm';
 import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 function App() {
@@ -7,16 +9,16 @@ function App() {
       <h1>CS4353 Web App</h1>
 
       <Router>
-        {/* Navigation bar */}
         <nav>
           <ul>
-            {/* Navigation link to the home page */}
             <li>
               <NavLink to="/"  className={({ isActive }) => isActive ? 'active' : undefined}>
                 Home
               </NavLink>
             </li>
-            {/* Navigation link to the Fuel Quote Form */}
+            <li>
+              <a href="/login">Login/Register</a>
+            </li>
             <li>
             <NavLink to="/fuel-quote-form" className={({ isActive }) => isActive ? 'active' : undefined}>
               Fuel Quote Form
@@ -27,7 +29,9 @@ function App() {
 
         {/* Routes */}
         <Routes>
-        <Route exact path="/" element={<h2>This is the main page</h2>} />
+          <Route exact path="/" element={<h2>This is the main page</h2>} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
           <Route exact path="/fuel-quote-form" element={<FuelQuoteForm />} />
         </Routes>
       </Router>
