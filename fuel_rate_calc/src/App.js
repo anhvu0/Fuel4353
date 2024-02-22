d4ea7f2f9e7295e084fba41a3d4fd7679268a89d
RyanPham9x <jamesryan_9x@yahoo.com>
2024-02-22 17:48:26 -0600

    MUI-signin-login

--- fuel_rate_calc/src/App.js
+++ fuel_rate_calc/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Login from './Login';
-import Register from './Register';
 import FuelQuoteForm from './FuelQuoteForm';
+import SignIn from './log-in';
+import SignUp from './SignUp';
 import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 function App() {
   return (
@@ -17,7 +17,9 @@ function App() {
               </NavLink>
             </li>
             <li>
-              <a href="/login">Login/Register</a>
+            <NavLink to="./log-in"  className={({ isActive }) => isActive ? 'active' : undefined}>
+                Log In
+              </NavLink>
             </li>
             <li>
             <NavLink to="/fuel-quote-form" className={({ isActive }) => isActive ? 'active' : undefined}>
@@ -30,8 +32,8 @@ function App() {
         {/* Routes */}
         <Routes>
           <Route exact path="/" element={<h2>This is the main page</h2>} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/log-in" element={<SignIn />} />
+          <Route exact path="/sign-up" element={<SignUp />} />
           <Route exact path="/fuel-quote-form" element={<FuelQuoteForm />} />
         </Routes>
       </Router>
