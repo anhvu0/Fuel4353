a733f502463acd4849c3f0452315df800ae2da06
RyanPham9x <jamesryan_9x@yahoo.com>
2024-02-22 03:12:08 -0600

    FuelQuoteFormv1

--- fuel_rate_calc/src/App.js
+++ fuel_rate_calc/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
+import FuelQuoteForm from './FuelQuoteForm';
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CS4353 Web App</h1>
+
+      <Router>
+        {/* Navigation bar */}
+        <nav>
+          <ul>
+            {/* Navigation link to the home page */}
+            <li>
+              <NavLink to="/"  className={({ isActive }) => isActive ? 'active' : undefined}>
+                Home
+              </NavLink>
+            </li>
+            {/* Navigation link to the Fuel Quote Form */}
+            <li>
+            <NavLink to="/fuel-quote-form" className={({ isActive }) => isActive ? 'active' : undefined}>
+              Fuel Quote Form
+            </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+        <Route exact path="/" element={<h2>This is the main page</h2>} />
+          <Route exact path="/fuel-quote-form" element={<FuelQuoteForm />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
+
 
 export default App;
