7c68c37d6d24805067bd582cf5f566e8e6f3ffba
Anh Vu <anhvu@anhvu.hsd1.tx.comcast.net>
2024-02-23 12:46:50 -0600

    include customer_profile

--- fuel_rate_calc/src/App.js
+++ fuel_rate_calc/src/App.js
@@ -3,6 +3,7 @@ import Login from './Login';
 import Register from './Register';
 import FuelQuoteForm from './FuelQuoteForm';
 import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import CustomerProfile from './customer_profile';
 function App() {
   return (
     <div className="App">
@@ -33,6 +34,7 @@ function App() {
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/fuel-quote-form" element={<FuelQuoteForm />} />
+          <Route exact path="/customer-profile" element={<CustomerProfile />} />
         </Routes>
       </Router>
     </div>
