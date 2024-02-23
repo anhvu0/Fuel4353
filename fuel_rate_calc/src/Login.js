e0b85762a567c42e0bd2f83953c3ddc408714d13
Khoa Mai <97200413+KhoaMai1306@users.noreply.github.com>
2024-02-22 21:48:14 -0600

    Re-direct Register to login

--- fuel_rate_calc/src/Login.js
+++ fuel_rate_calc/src/Login.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './FuelQuoteForm.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./FuelQuoteForm.css";
 
 const Login = () => {
   return (
     <div>
-        <div className="title">
-            <h2>Login</h2>
-        </div>
-        <div className="form-container">
-        <form className="fuel-quote-form"> 
-            <div className="form-group">
+      <div className="title">
+        <h2>Login</h2>
+      </div>
+      <div className="form-container">
+        <form className="fuel-quote-form">
+          <div className="form-group">
             <label htmlFor="username">Username:</label>
             <input type="text" id="username" className="form-control" />
-            </div>
-            <div className="form-group">
+          </div>
+          <div className="form-group">
             <label htmlFor="password">Password:</label>
             <input type="password" id="password" className="form-control" />
-            </div>
-  
-            <div className="button-container">
-                <button type="submit" className="button-login">Login</button>
-                
-            </div>
-            <div className="button-container">
-                <Link to="/register" style={{ textDecoration: 'none' }}>
-                        <button type="button" className="button-register">Register</button>
-                    </Link>
-            </div>
+          </div>
+
+          <div className="button-container">
+            <button type="submit" className="button-login">
+              Login
+            </button>
+          </div>
+          <div className="button-container">
+            <Link to="/register" style={{ textDecoration: "none" }}>
+              <button type="button" className="button-register">
+                Register
+              </button>
+            </Link>
+          </div>
         </form>
-        </div>
+      </div>
     </div>
   );
 };
