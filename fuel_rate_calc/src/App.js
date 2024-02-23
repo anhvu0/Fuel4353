243a31bdb0d16da442aec75ccf0dc2a2c364f4dc
Anh Vu <anhvu@anhvu.hsd1.tx.comcast.net>
2024-02-23 17:09:12 -0600

    new commits

--- fuel_rate_calc/src/App.js
+++ fuel_rate_calc/src/App.js
@@ -6,10 +6,45 @@ import FuelQuoteForm from './FuelQuoteForm';
 import FuelQuoteHistory from './FuelQuoteHistory';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import CustomerProfile from './customer_profile';
+import { NavLink } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Router>
+      <header>
+        <h1>Smart Fuel Rates</h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/"  className={({ isActive }) => isActive ? 'active' : undefined}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              {/*<a href="/login">Login/Register</a>*/}
+              <NavLink to="/login"  className={({ isActive }) => isActive ? 'active' : undefined}>
+              Login/Register
+              </NavLink>
+            </li>
+            <li>
+            <NavLink to="/fuel-quote-form" className={({ isActive }) => isActive ? 'active' : undefined}>
+              Fuel Quote Form
+            </NavLink>
+            </li>
+            <li>
+            <NavLink to="/fuel-quote-history" className={({ isActive }) => isActive ? 'active' : undefined}>
+              Quote History
+            </NavLink>
+            </li>
+            <li>
+            <NavLink to="/customer-profile" className={({ isActive }) => isActive ? 'active' : undefined}>
+              Customer Profile will be handled after logging in
+            </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <h2>Welcome to Our Service</h2>
+      </header>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/login" element={<Login />} />
--- fuel_rate_calc/src/HomePage.js
+++ fuel_rate_calc/src/HomePage.js
@@ -1,45 +1,10 @@
 import React from 'react';
 import './FuelQuoteForm.css';
 import mainPageImage from './img/mainpage.jpg';
-import { NavLink } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div>
-      <header>
-        <h1>Smart Fuel Rates</h1>
-        <nav>
-    <ul>
-      <li>
-        <NavLink to="/"  className={({ isActive }) => isActive ? 'active' : undefined}>
-          Home
-        </NavLink>
-      </li>
-      <li>
-        {/*<a href="/login">Login/Register</a>*/}
-        <NavLink to="/login"  className={({ isActive }) => isActive ? 'active' : undefined}>
-        Login/Register
-        </NavLink>
-      </li>
-      <li>
-      <NavLink to="/fuel-quote-form" className={({ isActive }) => isActive ? 'active' : undefined}>
-        Fuel Quote Form
-      </NavLink>
-      </li>
-      <li>
-      <NavLink to="/fuel-quote-history" className={({ isActive }) => isActive ? 'active' : undefined}>
-        Quote History
-      </NavLink>
-      </li>
-      <li>
-      <NavLink to="/customer-profile" className={({ isActive }) => isActive ? 'active' : undefined}>
-        Customer Profile will be handled after logging in
-      </NavLink>
-      </li>
-    </ul>
-  </nav>
-        <h2>Welcome to Our Service</h2>
-      </header>
       <main>
         <img src={mainPageImage} alt="Gas Station at Night" className="responsive-image" />
       </main>
