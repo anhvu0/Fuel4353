ac96bab1bec618630451d58dc3e527273ee36175
RyanPham9x <jamesryan_9x@yahoo.com>
2024-03-14 18:38:25 -0500

    update settings.py and requirements.txt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
-import environ
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()  # loads the configs from .env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,19 +24,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # os.environ.get('SECRET_KEY')
 #
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = str(os.getenv('DEBUG', 'False').lower() == 'true')
 # os.environ.get('DEBUG', 'False').lower() == 'true'
 # True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ') 
+ALLOWED_HOSTS = list(os.getenv('ALLOWED_HOSTS').split(' '))
 # os.environ.get('ALLOWED_HOSTS').split(' ') 
-# []
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -45,7 +46,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-database_url = os.environ.get('DATABASE_URL')   
+database_url = str(os.getenv('DATABASE_URL'))
 DATABASES["default"] = dj_database_url.parse(database_url)
 #os.environ.get('DATABASE_URL')
 
