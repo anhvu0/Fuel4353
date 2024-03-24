74d3f5ec5ac1882d191607627ce67c5a8e4013d1
RyanPham9x <jamesryan_9x@yahoo.com>
2024-03-23 19:29:59 -0500

    fix eslint warning

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,9 +6,7 @@ export default function Footer() {
     <MDBFooter className='text-center text-lg-left '>
       <div className='text-center p-3 text-secondary' style={{ backgroundColor: 'white' }}>
         &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a>
-          CS4353 Group 52
-        </a>
+        CS4353 Group 52
       </div>
     </MDBFooter>
   );
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,19 +1,19 @@
 import { createContext, useState, useEffect, useCallback } from 'react'
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify';
 const AuthContext = createContext()
 
 export default AuthContext;
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
 
     let [user, setUser] = useState(() => (sessionStorage.getItem('authTokens') ? jwtDecode(sessionStorage.getItem('authTokens')) : null))
     let [authTokens, setAuthTokens] = useState(() => (sessionStorage.getItem('authTokens') ? JSON.parse(sessionStorage.getItem('authTokens')) : null))
     let [loading, setLoading] = useState(true)
 
     const navigate = useNavigate()
-    
+
 
     let loginUser = async (e) => {
         e.preventDefault()
@@ -22,12 +22,12 @@ export const AuthProvider = ({children}) => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({username: e.target.username.value, password: e.target.password.value })
+            body: JSON.stringify({ username: e.target.username.value, password: e.target.password.value })
         });
 
         let data = await response.json();
 
-        if(response.ok){
+        if (response.ok) {
             sessionStorage.setItem('authTokens', JSON.stringify(data));
             setAuthTokens(data);
             setUser(jwtDecode(data.access));
@@ -39,63 +39,64 @@ export const AuthProvider = ({children}) => {
         }
     };
 
-    let logoutUser = (e) => {
+    const logoutUser = useCallback((e) => {
         //e.preventDefault()
         sessionStorage.removeItem('authTokens')
         setAuthTokens(null)
         setUser(null)
         setTimeout(() => navigate('/login'), 450);
         toast.dismiss();
-    }
+    }, [navigate]);
 
     const updateToken = useCallback(async () => {
         const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/token/refresh/`, {
             method: 'POST',
             headers: {
-                'Content-Type':'application/json'
+                'Content-Type': 'application/json'
             },
-            body:JSON.stringify({refresh:authTokens?.refresh})
+            body: JSON.stringify({ refresh: authTokens?.refresh })
         })
 
         const data = await response.json()
         if (response.status === 200) {
             setAuthTokens(data)
             setUser(jwtDecode(data.access))
-            sessionStorage.setItem('authTokens',JSON.stringify(data))
+            sessionStorage.setItem('authTokens', JSON.stringify(data))
         } else {
             logoutUser()
         }
 
-        if(loading){
+        if (loading) {
             setLoading(false)
         }
-    }, [authTokens, setAuthTokens, setUser, logoutUser])
+    }, [authTokens, setAuthTokens, setUser, logoutUser, loading])
 
     let contextData = {
-        user:user,
-        authTokens:authTokens,
-        loginUser:loginUser,
-        logoutUser:logoutUser,
+        user: user,
+        authTokens: authTokens,
+        loginUser: loginUser,
+        logoutUser: logoutUser,
     }
 
-    useEffect(()=>{
-        if(loading){
+    useEffect(() => {
+        if (loading) {
             updateToken()
         }
 
         const REFRESH_INTERVAL = 1000 * 60 * 4 // 4 minutes
-        let interval = setInterval(()=>{
-            if(authTokens){
+        let interval = setInterval(() => {
+            if (authTokens) {
                 updateToken()
             }
         }, REFRESH_INTERVAL)
         return () => clearInterval(interval)
 
-    },[authTokens, loading])
+    }, [authTokens, loading, updateToken])
 
-    return(
+    return (
         <AuthContext.Provider value={contextData}>
             {children}
         </AuthContext.Provider>
     )
-}
+}//Latest commit fixed eslint warning by adding the dependencies. 
+//Could cause potential bug. Revert this file if needed.
